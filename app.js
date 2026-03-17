5568a090f8f179b08b8009cc76c6394c8a76369b
ebarthelemydev <ebarthelemy@edenschool.fr>
2026-03-17 14:39:33 +0100

    feat
    feat

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-const $glowstone = document.querySelector("#glowstone");
-const $btn = document.querySelector("#light");
-const $modal = document.querySelector("dialog");
-const $dialog = document.querySelector("dialog");
-const $body = document.querySelector("body");
-const $off = document.querySelector("#menu-code-status");
+const $form = document.querySelector("#add-form");
+const $text = document.querySelector("#light-name");
 
 let etat = false;
 
-$btn.addEventListener("click", () => {
-	$modal.showModal();
-});
-
-$dialog.addEventListener("submit", () => {
-	if (etat == false) {
-		$body.classList.add("light-on");
-		$body.classList.remove("light-off");
-		$glowstone.src = "/assets/glowstone-on.webp";
-		etat = true;
-	} else {
-		$body.classList.remove("light-on");
-		$body.classList.add("light-off");
-		$glowstone.src = "/assets/glowstone-off.webp";
-		etat = false;
-	}
+$form.addEventListener("submit", (e) => {
+	e.preventDefault();
+    for (let i = 0; i < $text.length; i++) {
+        console.log("cc")
+        
+    }
 });
